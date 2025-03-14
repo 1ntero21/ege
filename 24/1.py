ccac3cd21d1d02871a728af368e3d1eadb948df7
1ntero21 <147915230+1ntero21@users.noreply.github.com>
2025-03-14 10:57:06 +0700

    Add files via upload

---
+++ 24/1.py
@@ -0,0 +1,35 @@
+def f(lst):
+    for i in lst:
+        if len(i) == 10:
+            sum_num = sum(map(int, i))
+            if sum_num**0.5 == int(sum_num**0.5):
+                return True
+    return False
+    
+
+with open('1.txt') as fin:
+    s = fin.readline().strip()
+
+max_len = 0
+subs = ''
+
+for i in range(len(s)):
+    digit = s[i]
+    if digit.isdigit():
+        subs = digit
+        while subs:
+            i += 1
+            if i >= len(s):
+                break
+            if s[i].isdigit():
+                subs += s[i]
+            else:
+                if subs[-1].isdigit():
+                    subs += s[i]
+                else:
+                    subs = subs[:-1]
+                    if f(subs.split('=')):
+                        max_len = max(max_len, len(subs))
+                    subs = ''
+            
+print(max_len)
